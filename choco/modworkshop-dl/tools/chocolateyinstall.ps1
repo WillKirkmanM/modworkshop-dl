c30ac2eff4c384894dd864b558571b7a7d0b8c09
Will <WillKirkmanM@gmail.com>
2023-01-24 08:44:41 +0000

    Feat: Chocolatey Package

---
+++ choco/modworkshop-dl/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/WillKirkmanM/modworkshop-dl/releases/download/v1.0.0/modworkshop-dl.exe' # download url, HTTPS preferred
+$url64      = 'https://github.com/WillKirkmanM/modworkshop-dl/releases/download/v1.0.0/modworkshop-dl.exe' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+
+$packageArgs = @{
+  packageName   = 'modworkshop-dl'
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'modworkshop-dl*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  checksum      = ''
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  checksum64    = ''
+  checksumType64= 'sha256' #default is checksumType
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+  }
+
+Install-ChocolateyPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateypackage
