eb1431aa4bbd9753f5287be98b80d9f5aa002998
WillKirkmanM <WillKirkmanMoller@gmail.com>
2023-10-17 18:23:55 +0100

    Feat(ListOfMods.go): Implemented Mod List Function

--- pkg/search/ListOfMods.go
+++ pkg/search/ListOfMods.go
@@ -1,5 +1,37 @@
 package search
 
+import (
+	"fmt"
+)
+
 func ListOfMods(args []string) {
-	Search(args)
+	modList := Search(args)
+
+	for i, mod := range modList.Data{
+			if mod.Category.Name != "" {
+				mod.Category.Name = "/" + " " + mod.Category.Name
+			}
+
+			fmt.Printf(
+				`
+		[%v] %s 
+			🧍 %s
+			📍 %s %s
+			❤  %v 😋 %v 👁️  %v		🕑 %s
+		
+		`, i+1,
+				mod.Name,
+				mod.User.Name,
+
+				mod.Game.Name, 
+				mod.Category.Name,
+
+				mod.Likes,
+				mod.Downloads,
+				mod.Views,
+
+				// parse the mod.UpdatedAt time relative like 10 days ago
+				mod.UpdatedAt.Local().Format("Jan 2, 2006"),
+			)
+		}
 }
